e9f3edf65f9bbf0ab9b2e69fb43c867328ff988b
junpei <s13005@s13005.it-college.local>
2014-08-14 16:09:16 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/it_college/std/s13005/androidchallenge/GameActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.it_college.std.s13005.androidchallenge;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+public class GameActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
