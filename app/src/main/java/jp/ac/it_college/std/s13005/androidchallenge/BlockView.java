2f03f3ff3fea06e94fddea481bed89aadc4975b4
s13005 <s13005@std.it-college.ac.jp>
2014-08-22 14:34:52 +0900

    commit6

--- app/src/main/java/jp/ac/it_college/std/s13005/androidchallenge/BlockView.java
+++ app/src/main/java/jp/ac/it_college/std/s13005/androidchallenge/BlockView.java
@@ -7,44 +7,48 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.view.View;
 
 import java.util.Random;
 
-public class BlockView extends View{
-    public BlockView(Context context) {super(context);}
+public class BlockView extends View {
+    public BlockView(Context context) {
+        super(context);
+    }
 
     Random random = new Random(System.currentTimeMillis());
 
     int[][][] blocks = {
             {
-                    {1,1},
-                    {0,1},
-                    {0,1}
+                    {1, 1},
+                    {0, 1},
+                    {0, 1}
             },
             {
-                    {1,1},
-                    {1,0},
-                    {1,0}
+                    {1, 1},
+                    {1, 0},
+                    {1, 0}
             },
             {
-                    {1,1},
-                    {1,1}
+                    {1, 1},
+                    {1, 1}
             },
             {
-                    {1,0},
-                    {1,1},
-                    {1,0}
+                    {1, 0},
+                    {1, 1},
+                    {1, 0}
             },
             {
-                    {1,0},
-                    {1,1},
-                    {0,1}
+                    {1, 0},
+                    {1, 1},
+                    {0, 1}
             },
             {
-                    {0,1},
-                    {1,1},
-                    {1,0}
+                    {0, 1},
+                    {1, 1},
+                    {1, 0}
             },
             {
                     {1},
@@ -54,25 +58,4 @@ public class BlockView extends View{
             }
     };
 
-    int[][] block = blocks[random.nextInt(blocks.length)];
-    int posx, posy;
-    int mapWidth = 20;
-    int mapHeight = 20;
-    int[][] map = new int[mapHeight][];
-
-
-/*
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-
-        Rect rect = new Rect(50, 100, 150, 200);
-        canvas.drawRect(rect, paint);
-
-    }
-*/
 }
-
